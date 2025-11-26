82c44812cac13fb7a3aa677f8c004116765526d5
AndreaQuelali <andrealizbethquelali5@gmail.com>
2025-11-25 23:56:34 -0400

    feat: add React and related dependencies to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
+import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  integrations: [icon()],
+  integrations: [
+    icon(),
+    react(),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
